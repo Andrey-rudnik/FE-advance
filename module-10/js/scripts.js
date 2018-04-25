79a4eeb159fa0385515afbc6cbc9015b922fcf80
Andrey-rudnik <andrey-rudnik199122sony@rambler.ru>
2018-04-25 21:10:41 +0300

    HW10

--- module-10/js/scripts.js
+++ module-10/js/scripts.js
@@ -22,6 +22,46 @@
 // дается строка и от первого нажатия до посленего
 // правильного набранного знака считать время
 const lang = "qwerty";
-const string = "qryte";
-const charsArr = string.split("").reverse();
-const timerOutput = document.querySelector(".timer");
+const string = "qryte"; //строка которая нам нужна(которую нужно будет вводить для проверки)
+const charsArr = string.split("").reverse(); //массив строки которую нужно вводить
+const timerOutput = document.querySelector(".timer"); //путь на див где будет таймер
+const exerciseOutput = document.querySelector(".exercise"); //путь на див где будет выведена строка string
+const keyboard = document.querySelector(".keyboard"); // путь где будет виден результат нашей игры
+
+let arrMain = []; //создаем массив где будут хранится все верно введенные буквы
+let counter = 0; //создаем счетчик для таймера
+let allKeys = []; //создаем массив где будут хранится все буквы которые мы ввели(не только те что есть в string)
+let prob = [];
+
+
+exerciseOutput.innerHTML = string;
+keyboard.textContent = `Your last result: 5 letters in ${localStorage.getItem("data")} sec`;
+
+let time = setInterval(() => {
+    counter++;
+    timerOutput.innerHTML = counter;
+}, 1000);
+
+window.addEventListener("keydown", function(e) {
+    if (allKeys.length < 5) {
+        allKeys.push(e.key);
+        prob.push(e.key);
+        if (charsArr.includes(e.key)) {
+            arrMain.push(e.key);
+            if (arrMain.length == 5) {
+                for (let i = 0; i < arrMain.length; i++) {
+                    let j = i + 1;
+                    if (arrMain[i] == charsArr[i] && prob[i] != allKeys[0]) {
+                        clearInterval(time);
+                        keyboard.textContent = "";
+                        keyboard.textContent = `Your result: 5 letters in ${counter} sec`;
+                        localStorage.setItem("data", JSON.stringify(counter));
+                    }
+                }
+            }
+        }
+    } else {
+        clearInterval(time);
+        keyboard.textContent = "Try again!";
+    }
+});
